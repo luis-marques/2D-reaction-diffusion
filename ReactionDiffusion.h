2be29824c0183797c76bbef717fbf754175b9670
Luis Marques <lf1119@ic.ac.uk>
2022-03-18 23:46:15 +0000

    Boost proper error handling.

--- ReactionDiffusion.h
+++ ReactionDiffusion.h
@@ -9,11 +9,11 @@
  */
 class ReactionDiffusion {
     
-    // Should be private?
-    //private:
+    // Encapsulating solution fields u,v and other problem parameters.
+    // Ensures they can only be accessed and modified by class methods.
     private:
     
-        // Class variables (should be static const??)
+        // Problem parameters (should be static const??)
         double dt;
         int T;
         int Nx;         // Number of grid-points/nodes along x direction
@@ -24,38 +24,33 @@ class ReactionDiffusion {
         double mu2;
         double eps;
         
-        
+        // Variables defined to improve performance.
         double recip_a;
         double u_grad_coef;
         double v_grad_coef;
         double u_squared;
         double dt_eps;
         double b_over_a;
-        
         int nr_timesteps;
                 
-        // Solutions
+        // Solution fields.
         double* u;
         double* v;
         double* u_prev;
         double* v_prev;
         
-        
+    // Methods are public as these must be accessed outside.
     public:
-        // Constructor as want to set the variables as constant and that can't be done if they are 
-        // initialized in SetParams
         void SetParameters(const double& arg_dt, const int& arg_T,
                            const int& arg_Nx, const int& arg_Ny,
                            const double& arg_a, const double& arg_b,
                            const double& arg_mu1, const double& arg_mu2,
                            const double& arg_eps);
 
-        
         void SetInitialConditions();
-                
         void TimeIntegrate();
-                
         void Terminate();
+        
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
  * Author: CID-01715222.
  */
 #include <iostream>
-#include <iomanip>
 
 #include <boost/program_options.hpp>
 
@@ -19,50 +18,55 @@ namespace po = boost::program_options;
 int main(int argc, char* argv[]) {
     // Specify the options we want to make available to the user
     po::options_description opts("Allowed options");
+    // dt and T are only required parameters since they do not appear in Table 1.
     opts.add_options()
-        ("dt", po::value<double>(),
-                 "Time-step to use.")
-        ("T",  po::value<int>(),
-                 "Total integration time.")
+        ("dt", po::value<double>()->required(),
+                 "Time-step to use. (double)")
+        ("T",  po::value<int>()->required(),
+                 "Total integration time. (int)")
         ("Nx",  po::value<int>()->default_value(101),
-                 "Number of grid points in x.")
+                 "Number of grid points in x. (int)")
         ("Ny",  po::value<int>()->default_value(101),
-                 "Number of grid points in y.")
+                 "Number of grid points in y. (int)")
         ("a",  po::value<double>()->default_value(0.75),
-                 "Value of parameter a.")
+                 "Value of parameter a. (double)")
         ("b",  po::value<double>()->default_value(0.06),
-                 "Value of parameter b.")
+                 "Value of parameter b. (double)")
         ("mu1",  po::value<double>()->default_value(5.0),
-                 "Value of parameter mu1.")
+                 "Value of parameter mu1. (double)")
         ("mu2",  po::value<double>()->default_value(0.0),
-                 "Value of parameter mu2.")
+                 "Value of parameter mu2. (double)")
         ("eps",  po::value<double>()->default_value(50.0),
-                 "Value of parameter epsilon.")
-        ("help",       "Print help message.");
+                 "Value of parameter epsilon. (double)")
+        ("help", "Prints the help message.")
+
+        ;
 
     // Tell Boost to parse the command-line arguments using the list of
     // possible options and generate a map (vm) containing the options and
     // values actually specified by the user.
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, opts), vm);
-    po::notify(vm);
+    
+    // Handles errors with the passed arguments and provides helpful error messages.
+    try {
+        po::store(po::parse_command_line(argc, argv, opts), vm);
+        
+        // Check for if user asked for help or if no arguments were supplied.
+        if (vm.count("help") || argc == 1) {
+            cout << "Please provide a value for all the required options (optional options are indicated with round brackets)." << endl
+                 << opts << endl;
+            return 1;
+        }
+        
+        po::notify(vm);
 
-    // Check if the user gave the "--help" option and print the usage.
-    if (vm.count("help") || argc == 1) {
-        cout << "Please provide a value for the required options (indicated with round brackets)." << endl
-             << opts << endl;
-        return 0;
+    } catch (exception& e) {
+        cout << "Error: " << e.what() << endl;
+        cout << opts << endl;
+        return 1;
     }
-
-    // Extract the values given to other parameters using the appropriate
-    // data type.
-//    try {
-//        const double dt = vm["dt"].as<double>();
-//    }
-//    catch {
-//        cout << "ERROR" << endl;
-//        return 1;
-//    }
+    
+    // Extracts values given to parameters using the appropriate datatype.
     const double dt = vm["dt"].as<double>();
     const int T = vm["T"].as<int>();
     const int Nx = vm["Nx"].as<int>();
@@ -72,6 +76,7 @@ int main(int argc, char* argv[]) {
     const double mu1 = vm["mu1"].as<double>();
     const double mu2 = vm["mu2"].as<double>();
     const double eps = vm["eps"].as<double>();
+
     
     // Initializing Reaction Diffusion class
     ReactionDiffusion react_diff;
@@ -79,10 +84,13 @@ int main(int argc, char* argv[]) {
     // Setting the problem parameters as specific in the command-line
     react_diff.SetParameters(dt, T, Nx, Ny, a, b, mu1, mu2, eps);
     
+    
     react_diff.SetInitialConditions();
+    
     
     react_diff.TimeIntegrate();
     
+
     react_diff.Terminate();
     
     return 0;
